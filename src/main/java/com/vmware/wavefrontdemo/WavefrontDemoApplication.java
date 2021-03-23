d106196b8d0d447157dfcc5147ef1ff6df573009
Declan McAleese <21492+djmcaleese@users.noreply.github.com>
2021-03-23 10:38:04 +0000

    Initial commit of project generated by start.spring.io
    https://start.spring.io/#!type=maven-project&language=java&platformVersion=2.4.4.RELEASE&packaging=jar&jvmVersion=16&groupId=com.vmware&artifactId=springboot-wavefront-demo&name=wavefront-demo&description=Demo%20of%20Spring%20Boot%20shipping%20metrics%20and%20traces%20to%20Wavefront&packageName=com.vmware.wavefrontdemo&dependencies=data-r2dbc,h2,webflux,cloud-starter-sleuth,lombok,wavefront

---
+++ src/main/java/com/vmware/wavefrontdemo/WavefrontDemoApplication.java
@@ -0,0 +1,13 @@
+package com.vmware.wavefrontdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WavefrontDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WavefrontDemoApplication.class, args);
+	}
+
+}
